7d9c67fabf2c4269ac76f2283ef553f6634fee51
SpringDream0406 <springdream0406@gmail.com>
2025-06-01 04:51:01 +0900

    nginx

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,15 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { setupSwagger } from "./commons/swagger/setupSwagger";
 // import { HttpExceptionFilter } from './commons/filter/http-exception.filter';
-import * as fs from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('/app/ssl/key.key'),
-    cert: fs.readFileSync('/app/ssl/cert.crt'),
-  };
-  
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   // app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
 
   setupSwagger(app);
 
-  await app.listen(3333);
+  await app.listen(3000);
 }
 bootstrap();
